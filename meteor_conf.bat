78b5f8be7f287b43e28e46b21f6f0bb6f1acab80
unknown <giovanni.massafra@ablio.it>
2017-10-26 15:48:17 +0200

    first commit

---
+++ meteor_conf.bat
@@ -0,0 +1,6 @@
+meteor add cordova:cordova-plugin-whitelist@1.3.1
+meteor add cordova:cordova-plugin-console@1.0.5
+meteor add cordova:cordova-plugin-statusbar@2.2.1
+meteor add cordova:cordova-plugin-device@1.1.4
+meteor add cordova:ionic-plugin-keyboard@1.1.4
+meteor add cordova:cordova-plugin-splashscreen@4.0.1
